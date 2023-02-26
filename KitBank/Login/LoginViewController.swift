12be061fd854ecd265bbcdb0c8b1301884ba82a8
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-02-26 16:26:34 +0200

    Sign in button added

--- KitBank/Login/LoginViewController.swift
+++ KitBank/Login/LoginViewController.swift
@@ -3,6 +3,14 @@ import UIKit
 class LoginViewController: UIViewController {
     
     let loginView = LoginView()
+    let signInButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.configuration = .filled()
+        button.configuration?.imagePadding = 8
+        button.setTitle("Sign In", for: [])
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,16 +22,31 @@ class LoginViewController: UIViewController {
 extension LoginViewController {
     private func style(){
         loginView.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.addTarget(self, action: #selector(signInTapped), for: .primaryActionTriggered)
     }
     
     private func layout(){
         view.addSubview(loginView)
+        view.addSubview(signInButton)
+        
         NSLayoutConstraint.activate([
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
-            
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1),
+        
+            signInButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: signInButton.trailingAnchor, multiplier: 1),
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2)
         ])
+    }
+    
+   
+}
+
+//MARK: - Actions
+extension LoginViewController{
+    @objc func signInTapped(sender: UIButton){
+        
     }
 }
 
