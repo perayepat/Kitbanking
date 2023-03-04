b90e50159580c7e5e079bdf0c86a478eb0b14d68
Mphathi Maapola <perrierpat@icloud.com>
2023-03-04 17:23:52 +0200

    Update UIViewcontroller+Utils.swift

--- KitBank/Utils/UIViewcontroller+Utils.swift
+++ KitBank/Utils/UIViewcontroller+Utils.swift
@@ -13,6 +13,13 @@ extension UIViewController{
         statusbarView.backgroundColor = appColor
         view.addSubview(statusbarView)
     }
+    func setStatusBar() {
+         let navBarAppearance = UINavigationBarAppearance()
+         navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+         navBarAppearance.backgroundColor = appColor
+         UINavigationBar.appearance().standardAppearance = navBarAppearance
+         UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+     }
     
     func setTabBarImage(imageName: String, title: String){
         let configuration = UIImage.SymbolConfiguration(scale: .large)
