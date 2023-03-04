90ed50293ef290279f127434dfaec68121ff4d16
Mphathi Maapola <perrierpat@icloud.com>
2023-03-04 12:33:06 +0200

    Ending onboarding by knowing which page is the last one

--- KitBank/AppDelegate.swift
+++ KitBank/AppDelegate.swift
@@ -6,20 +6,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     let loginViewController = LoginViewController()
+    let onboardingViewController = OnboardingContainerViewController()
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
         loginViewController.delegate = self // send us any signals we get
-//        window?.rootViewController = OnboardingContainerViewController()
-                window?.rootViewController = loginViewController
+        onboardingViewController.delegate = self
+        window?.rootViewController = onboardingViewController
+//                window?.rootViewController = loginViewController
         return true
     }
 }
 
-extension AppDelegate: LoginViewControllerDelegate{
+extension AppDelegate: LoginViewControllerDelegate, OnboardingContainerViewControllerDelegate{
+    func didFinishOnboarding() {
+        print("od -Done Onboarding")
+    }
+    
     func didLogin() {
-        print("Did Login")
+//        print("foo -Did Login")
+        
     }
 }
