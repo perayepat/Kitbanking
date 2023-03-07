4f91dd76b2af5dc8b5cc1cf6fd8f20361406b5fd
Mphathi Maapola <perrierpat@icloud.com>
2023-03-07 07:47:17 +0200

    tracking?

--- KitBank/Utils/UIViewcontroller+Utils.swift
+++ KitBank/Utils/UIViewcontroller+Utils.swift
@@ -8,6 +8,7 @@ extension UIViewController{
     func setSystemBar(){
         let statusBarSize = UIApplication.shared.statusBarFrame.size
         let frame = CGRect(origin: .zero, size: statusBarSize)
+        
         let statusbarView = UIView(frame: frame)
         
         statusbarView.backgroundColor = appColor
