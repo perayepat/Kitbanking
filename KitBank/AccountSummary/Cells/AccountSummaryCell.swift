ed9f3f7ad55ab163c056c766f046e7b731046d3e
Mphathi Maapola <perrierpat@icloud.com>
2023-03-07 07:58:03 +0200

    Summary cell and houseKeeping done

---
+++ KitBank/AccountSummary/Cells/AccountSummaryCell.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class AccountSummaryCell: UITableViewCell{
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension AccountSummaryCell{
+    private func setup(){
+        
+    }
+    private func layout(){
+        
+    }
+}
