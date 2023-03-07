d07caa76a5aef68e1aef351003a1fd9d7989f39b
Mphathi Maapola <perrierpat@icloud.com>
2023-03-07 08:30:48 +0200

    name Label added to table view cell

--- KitBank/AccountSummary/Cells/AccountSummaryCell.swift
+++ KitBank/AccountSummary/Cells/AccountSummaryCell.swift
@@ -4,6 +4,7 @@ class AccountSummaryCell: UITableViewCell{
     
     let typeLabel = UILabel()
     let divider = UIView()
+    let nameLabel = UILabel()
     static let reuseIdentifier = String(describing: AccountSummaryCell.self)
     
     enum cellConfiguration: CGFloat{
@@ -33,6 +34,12 @@ extension AccountSummaryCell{
         divider.translatesAutoresizingMaskIntoConstraints = false
         divider.backgroundColor = appColor
         
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        nameLabel.adjustsFontForContentSizeCategory = true
+        nameLabel.text = "No Fee All In Chequing"
+        
+        contentView.addSubview(nameLabel)
         contentView.addSubview(divider)
         contentView.addSubview(typeLabel) // In a table view cell the content view is the important view
     }
@@ -47,6 +54,12 @@ extension AccountSummaryCell{
             divider.topAnchor.constraint(equalToSystemSpacingBelow: typeLabel.bottomAnchor, multiplier: 1),
             divider.widthAnchor.constraint(equalToConstant: 60),
             divider.heightAnchor.constraint(equalToConstant: 4)
+        ])
+        
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: divider.leadingAnchor),
+            nameLabel.topAnchor.constraint(equalToSystemSpacingBelow: divider.bottomAnchor, multiplier: 2),
+            
         ])
     }
 }
