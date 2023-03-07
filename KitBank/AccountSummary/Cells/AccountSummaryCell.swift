bf7ac0804bc992ce8522c527affc71c0bb2589cb
Mphathi Maapola <perrierpat@icloud.com>
2023-03-07 08:41:07 +0200

    Balance label added to the cell completing the cell

--- KitBank/AccountSummary/Cells/AccountSummaryCell.swift
+++ KitBank/AccountSummary/Cells/AccountSummaryCell.swift
@@ -5,6 +5,10 @@ class AccountSummaryCell: UITableViewCell{
     let typeLabel = UILabel()
     let divider = UIView()
     let nameLabel = UILabel()
+    let balanceStackView = UIStackView()
+    let balanceLabel = UILabel()
+    let balanceAmountLabel = UILabel()
+    
     static let reuseIdentifier = String(describing: AccountSummaryCell.self)
     
     enum cellConfiguration: CGFloat{
@@ -37,11 +41,29 @@ extension AccountSummaryCell{
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.font = UIFont.preferredFont(forTextStyle: .body)
         nameLabel.adjustsFontForContentSizeCategory = true
-        nameLabel.text = "No Fee All In Chequing"
+        nameLabel.text = "Account Name"
         
         contentView.addSubview(nameLabel)
         contentView.addSubview(divider)
         contentView.addSubview(typeLabel) // In a table view cell the content view is the important view
+        
+        balanceStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        balanceLabel.translatesAutoresizingMaskIntoConstraints = false
+        balanceLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        balanceLabel.adjustsFontForContentSizeCategory = true
+        balanceLabel.text = "Some balance"
+        
+        balanceAmountLabel.translatesAutoresizingMaskIntoConstraints = false
+        balanceAmountLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        balanceAmountLabel.adjustsFontForContentSizeCategory = true
+        balanceAmountLabel.text = "$$$"
+        
+        contentView.addSubview(balanceStackView)
+        balanceStackView.addArrangedSubview(balanceLabel)
+        balanceStackView.addArrangedSubview(balanceAmountLabel)
+        balanceStackView.axis = .vertical
+        balanceStackView.alignment = .trailing
     }
     private func layout(){
         NSLayoutConstraint.activate([
@@ -59,7 +81,11 @@ extension AccountSummaryCell{
         NSLayoutConstraint.activate([
             nameLabel.leadingAnchor.constraint(equalTo: divider.leadingAnchor),
             nameLabel.topAnchor.constraint(equalToSystemSpacingBelow: divider.bottomAnchor, multiplier: 2),
-            
+        ])
+        
+        NSLayoutConstraint.activate([
+            trailingAnchor.constraint(equalToSystemSpacingAfter: balanceStackView.trailingAnchor, multiplier: 5),
+            balanceStackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 4)
         ])
     }
 }
